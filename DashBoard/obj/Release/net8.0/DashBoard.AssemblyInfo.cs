79fa21f0f9b8b7618f33ad68168776c98123c698
mongtritan <127721596+mongtritan@users.noreply.github.com>
2024-05-09 23:26:52 +0700

    Up1

--- DashBoard/obj/Release/net8.0/DashBoard.AssemblyInfo.cs
+++ DashBoard/obj/Release/net8.0/DashBoard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashBoard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa775514cfb6d22967ff232bbe6d942833fb6709")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee98cd860a1ea22e485f593e5448268c669c565c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashBoard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashBoard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
